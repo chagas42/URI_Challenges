c1205ae98f354bdfda7202b5b3324215efb05412
Junior Chagas <jjuniorchagas15@gmail.com>
2021-02-15 14:26:22 -0300

    challenge number 1002 completed

--- 1002/1002.js
+++ 1002/1002.js
@@ -4,10 +4,9 @@ const lines = input.split('\n');
 const circleRate = ( rate ) => {
     const n = 3.14159; 
     rate = rate * rate; 
-
     return n * rate; 
 }; 
 
-const result = circleRate(parseFloat(Number(lines)));
-console.log(`A=${result.toFixed(4)}`); 
+const result = circleRate(Number(lines[0]))
 
+console.log(`A=${result.toFixed(4)}`);
