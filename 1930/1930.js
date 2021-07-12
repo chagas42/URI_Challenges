1fa97cf28636ae75d22dd239ab78a3dd1284bc48
Celso Chagas <jjuniorchagas15@gmail.com>
2021-07-11 23:08:25 -0300

    finish challenge 1930

--- 1930/1930.js
+++ 1930/1930.js
@@ -1,4 +1,11 @@
-var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var input = require('fs').readFileSync('stdin', 'utf8');
 var lines = input.split('\n');
 
-console.log(lines); 
+let result, total = 0; 
+
+for(let i=0; i<lines[0].length; i++){
+    total += Number(lines[0][i]); 
+}
+
+result = total - 3; 
+console.log(result); 
